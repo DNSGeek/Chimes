825e7e50609b6a9172322f242f5e4f76cc0627de
DNSGeek <tknox@mac.com>
2024-01-27 15:21:18 -0800

    Update weather.py to pull AQI data

--- weather.py
+++ weather.py
@@ -1,73 +1,94 @@
-import requests
-import time
-from typing import Dict
-import prowl
+import logging
 import pickle
-import syslog
+import time
+from typing import Dict, Tuple
 
+import requests
 
-def log(msg: str) -> None:
-    syslog.openlog("pyWeather")
-    syslog.syslog(msg)
-    syslog.closelog()
+import prowl
 
-
-# Set this to your 5 digit zip code
-# You'll need to modify this and the get_weather URL for non-US
-ZIPCODE = ""
-
-# Set this to your openweather app id
-APPID = ""
-
-# Set these to your latitude and longitude
-LAT = ""
-LONG = ""
-
-# Change this directory to where you've installed this app
-BASEDIR = ""
+LAT: float = <MY_LATITUDE>
+LONG: float = <MY_LONGITUDE>
+ws: requests.Session = requests.Session()
 
 
 def get_weather() -> Dict:
-    url = f"https://api.openweathermap.org/data/2.5/weather?zip={ZIPCODE},us&"
-    url += f"appid={APPID}"
+    logging.debug("Getting current weather")
+    global ws
+    url = "https://api.openweathermap.org/data/2.5/weather?zip=95136,us&"
+    url += "appid=<MY_OPENWEATHERAPI_KEY_HERE"
     try:
-        r = requests.get(url, timeout=10)
+        r = ws.get(
+            url,
+            timeout=10,
+            headers={"X-Clacks-Overhead": "GNU Terry Pratchett"},
+        )
         retDict = r.json()
+        r.close()
         return retDict
     except Exception as ex:
-        log(f"Unable to get weather data: {ex}")
+        logging.error(f"Unable to get weather data: {ex}")
         return {}
 
 
-# Set MYWEATHERAPP and MYEMAIL to your values
+def get_aqi() -> Dict:
+    logging.debug("Getting current AQI")
+    global ws
+    url = f"https://api.airvisual.com/v2/nearest_city?lat={LAT}&lon={LONG}&"
+    url += "key=<MY_AIRVISUAL_KEY_HERE>"
+    try:
+        r = ws.get(
+            url,
+            timeout=10,
+            headers={"X-Clacks-Overhead": "GNU Terry Pratchett"},
+        )
+        retDict = r.json()
+        r.close()
+        return retDict
+    except Exception as ex:
+        logging.error(f"Unable to get AQI data: {ex}")
+        return {}
+
+
 def get_alerts() -> Dict:
-    headers = {"User-Agent": "MYWEATHERAPP, MYEMAIL"}
+    logging.debug("Getting current weather alerts")
+    global ws
+    headers = {
+        "User-Agent": "RPiClockWeather, <MY_EMAIL_ADD_HERE>",
+        "X-Clacks-Overhead": "GNU Terry Pratchett",
+    }
     url = "https://api.weather.gov/alerts/active?status=actual"
     url += f"&point={LAT},{LONG}"
     try:
-        r = requests.get(url, timeout=10, headers=headers)
+        r = ws.get(url, timeout=10, headers=headers)
         retDict = r.json()
+        r.close()
         return retDict
     except Exception as ex:
-        log(f"Unable to get alert data: {ex}")
+        logging.error(f"Unable to get alert data: {ex}")
         return {}
 
 
 def pull_alerts() -> None:
+    logging.debug("Entering pull_alerts")
     try:
-        oldalerts = pickle.load(open(f"{BASEDIR}/walerts.pickle", "rb"))
-    except Exception:
+        logging.debug("Reading the prior alerts")
+        oldalerts = pickle.load(open("/home/pi/Chimes/walerts.pickle", "rb"))
+    except Exception as ex:
+        logging.warning(f"Error reading existing alerts: {ex}")
         oldalerts = []
     alerts = get_alerts()
     if alerts == {}:
         return
     if "features" in alerts:
+        logging.info("Weather alerts found")
         for alt in alerts["features"]:
             alert = alt["properties"]
             if alert["status"] != "Actual":
                 continue
             if alt in oldalerts:
                 continue
+            logging.info(f"New alert {alert} found")
             oldalerts.append(alt)
             endtime = time.strptime(alert["expires"], "%Y-%m-%dT%H:%M:%S%z")
             starttime = time.strptime(alert["onset"], "%Y-%m-%dT%H:%M:%S%z")
@@ -82,11 +103,10 @@ def pull_alerts() -> None:
             else:
                 priority = 0
             prowl.sendAlert(
-                priority=priority,
-                msg=msg,
+                msg,
+                priority,
                 app="BigBen Weather Alerts",
                 event=subject,
-                url=alert["@id"],
             )
     else:
         alerts["features"] = []
@@ -96,19 +116,37 @@ def pull_alerts() -> None:
             continue
         newalerts.append(alert)
     try:
-        pickle.dump(newalerts, open(f"{BASEDIR}/walerts.pickle", "wb"), -1)
-    except Exception:
-        pass
+        pickle.dump(
+            newalerts, open("/home/pi/Chimes/walerts.pickle", "wb"), -1
+        )
+        logging.debug("Wrote new alerts file")
+    except Exception as ex:
+        logging.warning(f"Error writing alerts file: {ex}")
 
 
-def get_temperature() -> float:
+def get_temperature() -> Tuple[float, int]:
+    logging.debug("Entering get_temperature")
     weather = get_weather()
+    caqi = get_aqi()
+    # Only pull alerts at the top of the hour
     if time.localtime().tm_min < 5:
         pull_alerts()
+    curraqi = -1000
+    if "data" in caqi:
+        try:
+            curraqi = int(caqi["data"]["current"]["pollution"]["aqius"])
+            logging.debug(f"Current AQI is {curraqi}")
+        except Exception as ex:
+            logging.error(f"Unable to parse AQI: {ex}")
+            curraqi = -1000
     if "main" in weather:
         try:
-            return float(weather["main"].get("feels_like", 1273.15)) - 273.15
+            logging.debug(f"Current weather is {weather}")
+            return (
+                float(weather["main"].get("temp", -1273.15)) - 273.15,
+                curraqi,
+            )
         except Exception as ex:
-            log(f"Unable to get feels_like weather: {ex}")
-            return -1000.0
-    return -1000.0
+            logging.error(f"Unable to get weather: {ex}")
+            return (-1000.0, curraqi)
+    return (-1000.0, curraqi)
